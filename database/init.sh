db2639238d130caa20a80486fd6646fc85280057
Aayush Soni <Aayush.soni@mechlintech.com>
2025-05-09 01:55:53 +0530

    Rollback to commit f89ea2e6 (deployment issue fixing#12)

--- database/init.sh
+++ database/init.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# Wait for SQL Server to be up and running
-echo "Waiting for SQL Server to start..."
+# Wait for SQL Server to be ready
 sleep 30s
 
-echo "Running initialization SQL script..."
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "Pass@123" -i /init-db.sql
+# Run the initialization script
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "Pass@123" -i /docker-entrypoint-initdb.d/init-db.sql 
