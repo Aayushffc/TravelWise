5f88cdf716eccd7aa53a758f96f2ee570386411d
Aayush Soni <Aayush.soni@mechlintech.com>
2025-05-09 21:35:31 +0530

    Deployment issue fixing #14

--- database/init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Wait for SQL Server to be ready
-sleep 30s
-
-# Run the initialization script
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "Pass@123" -i /docker-entrypoint-initdb.d/init-db.sql 
